26b271eebefc1b09adab76ac1466cbc190fa2fd9
ZA139 <a1399677960@foxmail.com>
2020-04-27 01:46:08 +0800

    upload MouseClicker source code

---
+++ Intereing Little Toys/Intereing Little Toys/MouseClicker.cpp
@@ -0,0 +1,16 @@
+#include<Windows.h>
+int main(void) {
+	while (true)
+	{
+		if (GetAsyncKeyState(VK_SPACE)) {
+			while (true)
+			{
+				mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+				Sleep(100);
+				if (GetAsyncKeyState(VK_ESCAPE))return 0;
+				if (GetAsyncKeyState(VK_TAB))break;
+			}
+		}
+	}
+	return 0;
+}
